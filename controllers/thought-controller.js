98842bab7cc09c2220dec4bdac5d882b8ff9334f
Hannah Bush <heb.rutgers@gmail.com>
2022-05-18 12:03:39 -0400

    index added to routes to add /api prefix

---
+++ controllers/thought-controller.js
@@ -0,0 +1,94 @@
+const {Thought, User} = require('../models');
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({})
+    .select('-__v')
+    .sort({_id: -1})
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+  getThoughtById({params}, res) {
+    Thought.findOne({_id: params.id})
+    .select('-__v')
+    .then(dbThoughtData => {
+      if(!dbThoughtData){
+        res.status(404).json({message: 'No Thought found with this id!'});
+        return;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+  createThought({body}, res) {
+    Thought.create(body)
+    .then(({_id}) => {
+      return User.findByIdAndUpdate(
+        {_id: params.userId},
+        {$push: {thoughts: _id}},
+        {new: true}
+      );
+    })
+    .then(dbUserData => {
+      if(!dbUserData){
+        res.status(404).json({message: 'No User found with this id!'});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+  },
+  updateThought({params, body}, res) {
+    Thought.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+    .then(dbThoughtData => {
+      if(!dbThoughtData) {
+        res.status(404).json({message: 'No Thought found with this id!'});
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+  },
+  removeThought({params}, res) {
+    Thought.findOneAndDelete({_id: params.id})
+    .then(dbThoughtData => {
+      if(!dbThoughtData) {
+        res.status(404).json({message: 'No Thought found with this id!'});
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+  },
+  addReaction({params, body}, res) {
+    Thought.findByIdAndUpdate(
+      {_id: params.thoughtId},
+      {$push: {replies: body}},
+      {new: true, runValidators: true}
+    )
+    .then(dbReactionData => {
+      if (!dbReactionData) {
+        res.status(404).json({ message: 'No reaction found with this id!' });
+        return;
+      }
+      res.json(dbReactionData);
+    })
+    .catch(err => res.json(err));
+  },
+  removeReaction({params}, res) {
+    Thought.findByIdAndUpdate(
+      {_id: params.thoughtId},
+      {$pull: {replies: {reactionId: params.reactionId}}},
+      {new: true}
+    )
+    .then(dbReactionData => res.json(dbReactionData))
+    .catch(err => res.json(err));
+  }
+};
+
+module.exports = thoughtController;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes/api'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nosql-social-media-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
